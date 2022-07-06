ce27c24a315d708b5b007c48182cc6ad0bde837d
minwoogramer <01082249606nmw@gmail.com>
2022-07-07 01:00:35 +0900

    Utility functions, useAssets

--- App.js
+++ App.js
@@ -3,18 +3,36 @@ import React, { useState } from "react";
 import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
+//두개의 Utility functions을 만들었음
+// 한 배열을 넣어서 다른배열을 받고있음
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));//font들을 한번에 불러오고 싶을때 font는 Promise의 배열들이 될꺼임 
 
+const loadImages = (images) =>//string을 가져온다면 prefatch를 사용하고 그게아니라면 assets API 사용함
+//init db, get user avat, count notification같은 미리 받아오고 싶은것들이 있다면 함수를 만드는것이 사용자입장에서 좋다
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 export default function App() {
+  const [assets] = useAssets([require("./high.jpg")]);//이렇게하면 코드가 짧아지지만 원격 이미지를 불러올때 prefatch가 안됌
+  const [loaded] = Font.useFonts(Ionicons.font);
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./high.jpg"));//local에있는 파일 가져오기
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");//인터넷에 있는 이미지 가져오기
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./my-face.jpeg"),
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
+
       <AppLoading
         startAsync={startLoading}
         onFinish={onFinish}
