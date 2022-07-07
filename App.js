242532cbca2473d6cbb10876901164fe9dae44dd
minwoogramer <01082249606nmw@gmail.com>
2022-07-08 04:17:33 +0900

    type, styled

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
 import * as Font from "expo-font";
-import { Image } from "react-native";
+import { Image, useColorScheme } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Stack from "./navigation/Stack";
 import Root from "./navigation/Root";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./styled";
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 const loadImages = (images) =>
@@ -25,6 +27,7 @@ export default function App() {
     const fonts = loadFonts([Ionicons.font]);
     await Promise.all([...fonts]);
   };
+  const isDark = useColorScheme() === "dark";
   if (!ready) {
     return (
       <AppLoading
@@ -35,8 +38,10 @@ export default function App() {
     );
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
---
+++ screens/Movies.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Movies = ({ navigation: { navigate } }: any) => (
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title>Movies</Title>
+  </Btn>
+);
+export default Movies;
