90c56f9ce0654a50aaaa60fef6b02d1e7638df6c
minwoogramer <01082249606nmw@gmail.com>
2022-07-11 01:48:19 +0900

    refactor

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,20 +1,79 @@
-import React from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions } from "react-native";
+import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
+import Slide from "../components/Slide";
 
-const Btn = styled.TouchableOpacity`
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+
+const Container = styled.ScrollView``;
+
+const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor};
 `;
+const { height: SCREEN_HEIGHT } = Dimensions.get("window"); //화면에 알맞게 스크린 채워주기~
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Movies = ({ navigation: { navigate } }: any) => (
-  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title>Movies</Title>
-  </Btn>
-);
-export default Movies;
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const isDark = useColorScheme() === "dark";
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+      )
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
+    <Container>
+      <Swiper
+        horizontal
+        loop
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImgPath(movie.backdrop_path) }}
+            />
+            <BlurView
+              tint={isDark ? "dark" : "light"}
+              intensity={85}
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+                <Column>
+                  <Title isDark={isDark}>{movie.original_title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <Votes isDark={isDark}>⭐️ {movie.vote_average}/10</Votes>
+                  ) : null}
+                  <Overview isDark={isDark}>
+                    {movie.overview.slice(0, 100)}...
+                  </Overview>
+                </Column>
+              </Wrapper>
+            </BlurView>
+          </View>
+        ))}
+      </Swiper>
+    </Container>
+  );
+};
